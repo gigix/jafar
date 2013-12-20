839310debab02f1d4c4debfbea5e48bd158ee349
Jeff Xiong <gigix1980@gmail.com>
2013-12-20 13:41:19 +0300

    share common types between schemas

--- js/fhir-validator.js
+++ js/fhir-validator.js
@@ -3,11 +3,9 @@ exports.validate = function (jsonDocument, resourceName) {
         jsonDocument = JSON.parse(jsonDocument);
     }
 
-    var JaySchema = require('jayschema');
-    var jaySchema = new JaySchema();
-
     var fhirSchema = require(__dirname + "/fhir-schema.js");
     var resourceSchema = fhirSchema[resourceName];
 
-    return jaySchema.validate(jsonDocument, resourceSchema);
+    var JaySchema = require('jayschema');
+    return new JaySchema().validate(jsonDocument, resourceSchema);
 };
